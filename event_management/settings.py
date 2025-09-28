a38fce9b93e372a6e6cd0c81066e00cf34576a68
samiul haque <mdsamiulhaque682@gmail.com>
2025-09-28 15:21:52 +0600

    remove email info env file into setting for solve email sending problem.

--- event_management/settings.py
+++ event_management/settings.py
@@ -134,11 +134,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT", cast=int, default=587)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mdsamiulhaque682@gmail.com'
+EMAIL_HOST_PASSWORD = 'pbfb jdex ryyk joih' 
+
 
 FRONTEND_URL = 'http://127.0.0.1:8000'
 
